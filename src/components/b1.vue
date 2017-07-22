3a8825bec0b6282dcd8760c2de79c049518e0bdc
autor4n <alexey_orlov_94@mail.ru>
2017-07-22 13:17:54 +0300

    v 0.1

---
+++ src/components/b1.vue
@@ -0,0 +1,12 @@
+<template lang="jade">
+</template>
+
+<script>
+export default {
+  name: 'b1'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+</style>
---
+++ src/page.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+    .page
+</template>
+
+<script>
+import b1 from './components/b1'
+
+export default {
+  name: 'app',
+  components: {
+    b1
+  }
+}
+</script>
+
+<style src="./styles/global.sass" lang="sass" ></style>
